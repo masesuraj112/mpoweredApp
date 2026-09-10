e41246e920672c585bc2710e9ede7e9f4924f8df
masesuraj112 <157959335+masesuraj112@users.noreply.github.com>
2026-09-10 13:11:34 +1000

    Fixed input logic

--- src/components/mpowered/PainSlider.tsx
+++ src/components/mpowered/PainSlider.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { useState } from 'react';
+
 interface PainSliderProps {
   painLevel?: string;
   painDescription?: string;
@@ -8,11 +9,21 @@ interface PainSliderProps {
 
 
 export function PainSliderInput({painLevel, painDescription}: PainSliderProps) {
+  const [value, setValue] = useState(0);
+
+  // This function prevents input from being less than 0 or greater than 10
+  const changeNumber = (text: String) => {
+    const num = Number(text);
+    if (isNaN(num)) return 0;
+    setValue(Math.min(10, Math.max(0, num)));
+
+  }
+
   return (
     <View style={stylesSheet.container}>
       <Text style={stylesSheet.titleText}>Pain Intensity</Text>
       <Text style={stylesSheet.lineText}></Text>
-      <Text style={stylesSheet.painLevelText}>My {painLevel} pain is <TextInput style={stylesSheet.underlineText}/></Text>
+      <Text style={stylesSheet.painLevelText}>My {painLevel} pain is <TextInput style={stylesSheet.underlineText} keyboardType="number-pad" value={String(value)} onChangeText={changeNumber} /></Text>
 
     </View>
 
