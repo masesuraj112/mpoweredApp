2a4fe32d639d59bfb3241d6d09e69d19e9c4f277
masesuraj112 <157959335+masesuraj112@users.noreply.github.com>
2026-09-17 21:02:13 +1000

    Added pain summary test code

--- app/(auth)/demographics.tsx
+++ app/(auth)/demographics.tsx
@@ -12,7 +12,21 @@ import { scaleWidth, scaleHeight } from '@/services/scale';
 export default function Demographics() {
   return (
     <View style={styles.screen}>
-      {/* <PainSummary/> */}
+
+      {/* <PainSummary
+  summary={{
+    period: '18-24 May',
+    locations: ['Lower back', 'Other: Knee'],
+    characteristics: ['aching', 'throbbing'],
+    intensity: {
+      current: { value: 0, description: 'I do not experience pain at the moment.' },
+      mildest: { value: 2, description: 'I have experienced mild pain.' },
+      worst: { value: 9, description: 'My worst pain was very severe.' },
+      average: { value: 7, description: 'I have experienced severe pain.' },
+    },
+  }}
+  onClose={() => console.log('Closed')}
+/> */}
       
       {/* <PainSliderInput underLinedText={'worst pain'} bottomDescription={'The pain was fairly severe'} questionNumber={3}/>  */}
       <SingleChoiceInput />
