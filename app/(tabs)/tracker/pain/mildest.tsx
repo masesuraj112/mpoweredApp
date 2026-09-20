19d3e8f6ea6e4530400054f2e31ace343514827e
masesuraj112 <157959335+masesuraj112@users.noreply.github.com>
2026-09-20 11:00:12 +1000

    Built base of average, current, mildest, worst pages

--- app/(tabs)/tracker/pain/mildest.tsx
+++ app/(tabs)/tracker/pain/mildest.tsx
@@ -1,15 +1,34 @@
-import { View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { router } from 'expo-router';
 import { usePainAssessment } from '@/features/assessments/pain/context';
+import { ScaleSliderInput } from '@/components/mpowered/ScaleSlider';
+import { scaleFont } from '@/services/scale';
 
-export default function ScreenName() {
+
+export default function CurrePainLevel() {
   const { answers, updateAnswer } = usePainAssessment();
+  
 
   return (
-    <View>
-      <Text>Prompt text goes here</Text>
+    <View style={styles.screen}>
       {/* input component goes here */}
+      <Text style={styles.cardTitle}>My Pain</Text>
+      
+      <ScaleSliderInput underLinedText={'mildest pain'} titleText={'Pain Intensity'} assessmentType={'pain'} questionNumber={3}/> 
+
       <Button title="Next" onPress={() => router.push('/tracker/pain/worst')} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingHorizontal: 16,
+    paddingTop: 20, // extra top space to clear the status bar/notch area
+  },
+  cardTitle: {
+      fontSize: scaleFont(26),
+      fontWeight: '500',
+      marginBottom: 20
+  },
+});
